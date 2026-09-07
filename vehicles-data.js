1076ae7ca19f0c0e755e65fe491ffb374452f990
Sodium-Man <bhalaninaman7@gmail.com>
2026-09-08 09:27:15 +1000

    update: price@BMW X5 Black

--- vehicles-data.js
+++ vehicles-data.js
@@ -3,7 +3,7 @@ const vehicles = [
   id: "2015-bmw-x5-xdrive30d-black",
   title: "2015 BMW X5 xDrive30d F15 Auto 4x4",
   price: 20888,
-  kms: "156,605 km",
+  kms: "160,605 km",
   transmission: "Automatic",
   fuel: "Diesel",
   warranty: "12 month warranty included",
